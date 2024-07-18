e15ff7608f6ec95a964c692c89a3bed227e2533a
Mohamed BOUHADI <medbouhadi69@gmail.com>
2024-07-18 11:30:46 -0700

    Add files via upload

---
+++ Lessons from 038 to 046/Assignment 08/assignment8.cpp
@@ -0,0 +1,17 @@
+#include <array>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Your Code Here
+  array <int, 3> nums = {};
+
+  // Do Not Edit
+  nums.fill(100);
+  cout << nums[0] << "\n"; // 100
+  cout << nums[1] << "\n"; // 100
+  cout << nums[2] << "\n"; // 100
+
+  return 0;
+}
