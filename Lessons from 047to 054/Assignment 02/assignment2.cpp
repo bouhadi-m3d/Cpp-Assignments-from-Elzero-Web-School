16e5af9e9c6a5eb3d6fcef399bb012c80ed33a33
Mohamed BOUHADI <medbouhadi69@gmail.com>
2024-07-20 09:58:55 -0700

    Add files via upload

---
+++ Lessons from 047to 054/Assignment 02/assignment2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int index = 2;
+    for (;;) // <= Do Not Edit This Line
+        
+    {
+        cout << index << "\n"; // From 2 To 10
+        index++;
+        if (index > 8)
+        {
+            break;
+        }
+    }
+
+    // Output
+    // 2
+    // 3
+    // 4
+    // 5
+    // 6
+    // 7
+    // 8
+}
