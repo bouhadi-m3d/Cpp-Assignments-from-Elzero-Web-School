77e8f33ac3a0bc32661653224728f2b0b4a52143
Mohamed BOUHADI <medbouhadi69@gmail.com>
2024-07-20 23:44:07 -0700

    Add files via upload

---
+++ Lessons from 047 to 054/Assignment 05/assignment5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //For Loop
+    for (int i = 0; i <= 27; i += 3)
+    {
+        cout << i << endl;
+    }
+    // While Loop
+    cout << "=============\n";
+    int i = 0;
+    while (i <= 27)
+    {
+        cout << i << endl;
+        i += 3;
+    }
+    
+    return 0;
+}
