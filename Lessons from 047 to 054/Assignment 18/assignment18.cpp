0b7a979a2dcdba96bceba2e6e3bc0e0b28e076cf
Mohamed BOUHADI <medbouhadi69@gmail.com>
2024-07-23 15:56:38 -0700

    Add files via upload

---
+++ Lessons from 047 to 054/Assignment 18/assignment18.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <= 1000; i += 100) // Do Not Edit This Line
+    {
+        // Edit What You Need And Add Your Code
+        if (i > 0 && i < 1000 && i != 500)
+        {
+            cout << i << "\n";
+        }
+    }
+
+    // Output Needed
+    // 100
+    // 200
+    // 300
+    // 400
+    // 600
+    // 700
+    // 800
+    // 900
+    return 0;
+}
