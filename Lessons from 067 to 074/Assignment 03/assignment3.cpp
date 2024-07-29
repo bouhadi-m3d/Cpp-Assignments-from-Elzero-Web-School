22d3f4913088007c504f13e8fc2354a029a3bf4b
Mohamed BOUHADI <medbouhadi69@gmail.com>
2024-07-29 09:33:51 -0700

    Add files via upload

---
+++ Lessons from 067 to 074/Assignment 03/assignment3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> numbers = {10, 20, 30, 40, 50, 60, 70, 80};
+
+    // Write Your Code Here
+    vector<int>::iterator fir = numbers.begin();
+    vector<int>::iterator last = numbers.end() - 1;
+    
+    // Do Not Edit Below
+    cout << *fir << "\n";  // 10
+    cout << *last << "\n"; // 80
+
+    return 0;
+}
